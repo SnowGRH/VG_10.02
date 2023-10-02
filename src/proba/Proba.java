3cd6f761a9329caf5e976e6e97408ae05b1027ca
SnowGRH <gergelyovinczeff@gmail.com>
2023-10-02 13:56:07 +0200

    Nem mukodik

--- src/proba/Proba.java
+++ src/proba/Proba.java
@@ -3,26 +3,28 @@ package proba;
 import java.util.Scanner;
 
 public class Proba {
-    static  String[] Ujlapok;
+
+    static String[] Ujlapok;
+    static String[] lapok;
+
     public static void main(String[] args) {
-        String[] lapok = oszerak();
+        oszerak();
         kiir(lapok);
-        for (int i = 0; i < 3; i++) {
-            int oszlop = beolvas();
-            kever(oszlop,lapok);
-            kiir(Ujlapok);
-        }
+        int oszlop = beolvas();
+        kever(oszlop, lapok);
+        kiir(Ujlapok);
+
         ezvolt(Ujlapok);
     }
 
-    private static String[] kever(int oszlop,String[] lap) {
-        Ujlapok = new paklik[lap.length];
+    private static String[] kever(int oszlop, String[] lap) {
+        Ujlapok = new String[lap.length];
         switch (oszlop) {
             case 1:
                 for (int i = 0; i < 7; i++) {
-                    Ujlapok[i]= lap[20-(i-1)*3];
-                    Ujlapok[i+7]= lap[19-(i-1)*3];
-                    Ujlapok[i+14]= lap[21-(i-1)*3];
+                    Ujlapok[i] = lap[20 - (i - 1) * 3];
+                    Ujlapok[i + 7] = lap[19 - (i - 1) * 3];
+                    Ujlapok[i + 14] = lap[21 - (i - 1) * 3];
                 }
                 break;
         }
@@ -42,24 +44,27 @@ public class Proba {
             if (i / 3 == 0) {
                 System.out.println("");
             }
-            System.out.println(lapok[i]+" ");
+            System.out.println(lapok[i] + " ");
         }
 
     }
 
     private static String[] oszerak() {
-        String[] lapok = new paklik[22];
-        String[] szinek = {"piros", "tők","levél"};
-        String[] minták = {"XIII", "IX", "X", "kiraly"};
-        int j = 1;
-        for (int i = 0; i < 22 && j < lapok.length; i++) {
-            lapok[j++] = szinek[j] + "." + minták[i];
+        lapok = new String[22];
+        String[] szinek = {"piros", "tők", "levél"};
+        String[] mintak = {"XIII", "IX", "X", "kiraly"};
+        int j = 0;
+        for (String szin : szinek) {
+            for (int i = 0; i < 22 && j < szin.length(); i++) {
+                lapok[j++] = szinek[j] + "." + mintak[i];
+            }
         }
+
         return lapok;
     }
 
     private static void ezvolt(String[] lapok) {
-        System.out.println("ez a kártya volt: "+lapok[11] );
+        System.out.println("ez a kártya volt: " + lapok[11]);
     }
 
 }
